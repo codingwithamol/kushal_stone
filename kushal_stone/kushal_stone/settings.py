62026374684c5c3d213e5deec3a289dc4b27b0ef
MaheshWaghmare13 <sonuwaghmare7057@gmail.com>
2025-03-29 14:28:41 +0530

    database created

--- kushal_stone/kushal_stone/settings.py
+++ kushal_stone/kushal_stone/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'kushal_stone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ks',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PORT': 3306,
+        'PASSWORD': '',
+
     }
 }
 
