dcbeef1f804bff88159bc69e44c73df2186dff60
MaheshWaghmare13 <sonuwaghmare7057@gmail.com>
2025-03-31 14:43:22 +0530

    static created

--- kushal_stone/kushalapp/urls.py
+++ kushal_stone/kushalapp/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
    path('login/', views.login_view, name='login'),
    path('logout/', views.logout_view, name='logout'),
    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('sales_dashboard/', views.sales_dashboard, name='sales_dashboard'),
-    path('operations_dashboard/', views.operations_dashboard, name='operations_dashboard'),
-    path('manager_dashboard/', views.manager_dashboard, name='manager_dashboard'),
-    path('finance_dashboard/', views.finance_dashboard, name='finance_dashboard'),
+   path('sales_dashboard/', views.sales_dashboard, name='sales_dashboard'),
+   path('operations_dashboard/', views.operations_dashboard, name='operations_dashboard'),
+   path('manager_dashboard/', views.manager_dashboard, name='manager_dashboard'),
+   path('finance_dashboard/', views.finance_dashboard, name='finance_dashboard'),
    path('create_user/', views.create_user, name='create_user'), 
     
 ]
